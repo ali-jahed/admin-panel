897349584e437f1c95f3e5f2a85084c307456949
ali <aj.jahed1993@gmail.com>
2025-12-10 22:31:01 +0330

    initial project setup

---
+++ admin-backend/src/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const app = require("./app");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+console.log("MONGO_URI from env:", process.env.MONGO_URI);
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log("Server running on port " + PORT));
+  })
+  .catch((err) => console.log(err));
